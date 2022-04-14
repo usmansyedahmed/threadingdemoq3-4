9e64641af9114fd843b02c03e459cd30d51675bf
Syed Usman Ahmed <usman.ahmed@logiciel.com>
2022-04-14 02:58:54 -0700

    =More threading exercises

---
+++ ThreadingDemoQ3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+public class EntryPoint
+{
+    static private int count = 0;
+    static private object theLock = new Object();
+    private static void threadFunc()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                //int val = Interlocked.Increment(ref count);
+                Monitor.Enter(theLock);
+                count++;
+                Console.WriteLine(count + " printed by thread: " + Thread.CurrentThread.ManagedThreadId);
+            }
+            finally
+            {
+                Monitor.Exit(theLock);
+            }
+        }
+    }
+    public static void Main(string[] args)
+    {
+        IAsyncResult[] result = new IAsyncResult[10]; ;
+        Action countNumber = threadFunc;
+        for (int i = 0; i < 10; i++)
+        {
+            result[i] = countNumber.BeginInvoke(null, null);
+        }
+
+       for (int i = 0; i < 10; i++)
+       {
+            countNumber.EndInvoke(result[i]);
+       }
+
+    }
+
+}
---
+++ ThreadingDemoq4/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadingDemoq4
+{
+    public partial class Form1 : Form
+    {
+        private int threadFunc()
+        {
+            Random rndNum = new Random(Guid.NewGuid().GetHashCode());
+            int num = rndNum.Next();
+            return num;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Int32.Parse(textBox1.Text);
+            IAsyncResult[] result = new IAsyncResult[n];
+            int[] randNums = new int[n];
+            Func<int> getRandonNumber = threadFunc;
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = getRandonNumber.BeginInvoke(null, null);
+            }
+
+            textBox2.AppendText("Following random numbers are generated: ");
+            textBox2.AppendText(Environment.NewLine);
+
+            for (int i = 0; i < n; i++)
+            {
+                randNums[i] = getRandonNumber.EndInvoke(result[i]);
+                textBox2.AppendText(randNums[i].ToString());
+                textBox2.AppendText(Environment.NewLine);
+            }
+        }
+    }
+}
